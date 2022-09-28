b8da5c659c8775c85d69ec0e12e8bec7235313a0
muhedin998 <muhedin1998@gmail.com>
2022-09-28 03:34:31 +0200

    New models, redesigned reservations forms, design

--- social1/appointment/admin.py
+++ social1/appointment/admin.py
@@ -1,8 +1,11 @@
 from django.contrib import admin
-from .models import Korisnik,  Termin, Usluge
+from .models import Korisnik,  Termin, Usluge, Usluge, Frizer
 
 admin.site.register(Korisnik)
 
 admin.site.register(Termin)
 
 admin.site.register(Usluge)
+
+admin.site.register(Frizer)
+
