486e2ab925c24b9111583ad750b9cb0b1aff4db2
muhedin998 <muhedin1998@gmail.com>
2022-10-09 01:05:24 +0200

    removed unecceray data

--- social1/appointment/urls.py
+++ social1/appointment/urls.py
@@ -1,7 +1,6 @@
 from django.urls import path
 from . import views
 urlpatterns =[
-    path('', views.home, name='home'),
-    path('zakazi/', views.zakazi, name="zakazi")
+    path('', views.zakazi, name="zakazi"),
     
 ]
