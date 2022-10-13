01fe4162ee9c9c5c3208db5112aa08f389da8379
muhedin998 <muhedin1998@gmail.com>
2022-10-13 21:12:28 +0200

    Start building appintment scheduler

--- social1/appointment/views.py
+++ social1/appointment/views.py
@@ -7,7 +7,9 @@ from django.http import HttpResponse
 from datetime import datetime
 from .models import Usluge, Frizer
 
-def zakazi(request):
+def termin(request):
+    
+    viewname = "termin"
     sada = datetime.now()
 
     usluge = Usluge.objects.all()
@@ -33,6 +35,48 @@ def zakazi(request):
             #print(data)
 
     context = {
+        'viewname': viewname,
+        'godina': sada.year,
+        'mesec': format(sada.month, "02d"),
+        'dan': format(sada.day, "02d"),
+        'interval': interval,
+        'usluge': usluge,
+        'frizeri': frizeri,
+        'form': form,
+        'ls1':ls1,
+        'ls2':ls2,
+        "ls3":ls3,
+    }
+    return render(request, 'appointment/zakazivanje.html', context)
+
+def zakazi(request):
+    viewname = "zakazi"
+    sada = datetime.now()
+
+    usluge = Usluge.objects.all()
+    ls1=usluge[:round(len(usluge)/3)]
+    ls2= usluge[round(len(usluge)/3):round(len(usluge)/3+len(usluge)/3)]
+    ls3= usluge[round(len(usluge)/3+len(usluge)/3):]
+
+    frizeri = Frizer.objects.all()
+    form = TestForm()  
+    interval = 30
+    if request.method == 'POST':
+        for frizer in frizeri:
+            if frizer.name == request.POST["frizer"]:
+                print(frizer)
+        print(f'{request.POST["frizer"]} & {request.POST["usluga"]}')
+        data = TestForm(request.POST)
+        if data.is_valid():
+            data.save()
+            print("Form was VALID AND PASSED")
+            #print(data)
+        else:
+            print(f"From WAS NOT VALID ! -")
+            #print(data)
+
+    context = {
+        'viewname': viewname,
         'godina': sada.year,
         'mesec': format(sada.month, "02d"),
         'dan': format(sada.day, "02d"),
