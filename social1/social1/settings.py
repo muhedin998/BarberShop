1c481ef59be43070527167c7a20adfbaccffa2fd
muhedin998 <muhedin998@gmail.com>
2022-11-07 13:03:30 +0100

    Finished authenticating users

--- social1/social1/settings.py
+++ social1/social1/settings.py
@@ -85,12 +85,12 @@ DATABASES = {
     }
 }
 
-EMAIL_BACKEND = 'django.core.mail.backends.smtp.EmailBackend'
-EMAIL_HOST = "smtp.office365.com"
-EMAIL_HOST_USER = "muhedin998@gmail.com"
-EMAIL_HOST_PASSWORD = "Kokoska123!"
-EMAIL_PORT = 587
-EMAIL_USE_TLS = True
+#EMAIL_BACKEND = 'django.core.mail.backends.smtp.EmailBackend'
+# EMAIL_HOST = "smtp.gmail.com"
+# EMAIL_HOST_USER = "funygo36@gmail.com"
+# EMAIL_HOST_PASSWORD = "kokoska123"
+# EMAIL_PORT = 587
+# EMAIL_USE_TLS = True
 
 
 # Password validation
