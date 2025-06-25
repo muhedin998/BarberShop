9da6aff3397a6d9706a25f2f6919579b9f503d4e
muhedin998 <muhedin1998@gmail.com>
2025-06-26 01:27:34 +0200

    a lot of changes

---
+++ social1/appointment/context_processors.py
@@ -0,0 +1,13 @@
+from .models import Notification
+
+def notification_count(request):
+    """
+    Context processor to add unread notification count to all templates
+    """
+    if request.user.is_authenticated:
+        unread_count = Notification.objects.filter(
+            user=request.user,
+            is_read=False
+        ).count()
+        return {'unread_notification_count': unread_count}
+    return {'unread_notification_count': 0}
--- social1/appointment/views/views_admin.py
+++ social1/appointment/views/views_admin.py
@@ -1,4 +1,4 @@
-from django.contrib.auth.decorators import login_required
+from django.contrib.auth.decorators import login_required, user_passes_test
 from django.core.paginator import Paginator
 from django.shortcuts import render, redirect
 from ..forms import TestForm
@@ -129,6 +129,7 @@ def opcije_klijenti(request):
     return render(request, 'appointment/opcije/klijenti.html',{'users': users, 'count': count})
 
 
+@user_passes_test(lambda u: u.is_superuser)
 def opcije_izvestaj(request):
     today = datetime.now().date()
     duznici = Duznik.objects.all()
